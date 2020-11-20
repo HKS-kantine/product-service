ced9073d59d9d4338ebf6786e8a7450377f5d5f0
Maarten Jakobs <maarten.jakobs@gmail.com>
2020-11-20 12:28:52 +0100

    init

---
+++ AuthenticationData/CollectionAdminContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductData
+{
+    public class CollectionAdminContext : DbContext
+    {
+        private const string connectionString = "Server=localhost, 1400; Database=CollectionAdminDatabase; User Id=sa;Password=G4sLOj7P; MultipleActiveResultSets=true;";
+        private const string connectionStringDocker = "Server=db-server; Database=CollectionAdminDatabase; User Id=sa;Password=G4sLOj7P; MultipleActiveResultSets=true;";
+        public CollectionAdminContext()
+            : base()
+        {
+        }
+        public virtual DbSet<ProductEntities.ProductDTO> Products { get; set; }
+    }
+}
